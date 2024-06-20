0b9e6ffc994f8e005472678f83e939a8ab18c761
SiddharthMurjani <110025928+SiddharthMurjani@users.noreply.github.com>
2024-06-20 14:12:44 +0530

    Update hello.cob

--- hello.cob
+++ hello.cob
@@ -1,1 +1,20 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CheckVoterAge.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 VOTER-AGE        PIC 99.
+       01 AGE-MESSAGE      PIC X(30).
+
+       PROCEDURE DIVISION.
+       MAIN-PARA.
+           DISPLAY "Enter Voter's Age: " WITH NO ADVANCING
+           ACCEPT VOTER-AGE
+           IF VOTER-AGE > 18
+               MOVE "Eligible to Vote" TO AGE-MESSAGE
+           ELSE
+               MOVE "Not Eligible to Vote" TO AGE-MESSAGE
+           END-IF
+           DISPLAY AGE-MESSAGE
+           STOP RUN.
 
